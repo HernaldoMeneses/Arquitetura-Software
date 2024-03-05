e911040e867073cf8e0b2a4a217af395d36c2581
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 17:11:32 -0300

    1.1-Alem-da-Lingaugem.java

---
+++ 1-Plataforma-Java/1.1-Alem-da-Linguagem.java
@@ -0,0 +1,46 @@
+/* Lançada em 1995 pela Sun, sempre teve a ideia de uma plataforma de desenvolvimento e execução.
+*/
+
+/*
+    Plataforma
+        JVM - Java Virtual Machine
+            Conjunto de APIs e a linguagem Java
+            Abstrai não só a camada de hardware como a comunicação co o Sistema Operacional
+
+        JDK - Java Development Kit
+            Para Desenvolvedores Java
+        JRE - Java Runtime Environment
+            Para usuários Java
+    */
+
+/*
+    HVM - Hardware Virtual Machines
+        VMWare
+        Parallesl
+        VirtualBox
+            usados para rodar vários sistemas operacionais ao mesmo tempo.
+            utilizando o recurso de virtualização que abstrai o hardware da máquina.
+    */
+
+/*
+    Código C
+        Compilado (binarios)
+            Executáveis
+                Windows
+                Linux
+    */
+
+/*
+    Código Java
+        Compilado (binarios)
+            JVM - Java Virtual Machine (especifica para Windows ou Linux)
+                Executavel
+                    Windows
+                    Linux
+    */
+
+   /*
+   ver bytecodes Java
+   pag. 24/265
+   file:///L:/Linear_/Base-TI/Introdu%C3%A7%C3%A3o%20%C3%A0%20Arquitetura%20e%20Design%20de%20Software%20-%20Casa%20do%20Codigo.pdf
+   */
